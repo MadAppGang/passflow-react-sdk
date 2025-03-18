f2705c7939ec4c8eb320e47c900c7f7072e28379
Jack Rudenko <i@madappgang.com>
2025-03-18 22:04:12 +1100

    change js sdk reexport

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,4 @@ export * from './components/form';
 export * from './components/ui';
 export { PassflowProvider } from './components/provider';
 export * from './hooks';
-export { TokenType } from '@passflow/passflow-js-sdk';
-
-//reexport passflow-js-sdk
 export * from '@passflow/passflow-js-sdk';
