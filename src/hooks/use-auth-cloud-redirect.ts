9ca5d6ac96c4391fa1980ef4fb711840087df75a
Jack Rudenko <i@madappgang.com>
2025-03-18 23:24:48 +1100

    upgrade passflwo-js-sdk

--- src/hooks/use-auth-cloud-redirect.ts
+++ src/hooks/use-auth-cloud-redirect.ts
@@ -5,7 +5,7 @@ type UseAuthCloudRedirectProps = (cloudPassflowUrl: string) => () => void;
 export const useAuthCloudRedirect: UseAuthCloudRedirectProps = (cloudPassflowUrl: string) => {
   const passflow = usePassflow();
 
-  const redirect = () => passflow.authCloudRedirect(cloudPassflowUrl);
+  const redirect = () => passflow.authRedirect({ url: cloudPassflowUrl });
 
   return redirect;
 };
